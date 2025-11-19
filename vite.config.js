f3e7dde53af23d651c22dd3d2f2506bd6b405820
KongHou <2784332934@qq.com>
2025-11-19 19:50:08 +0800

    feat: fix dist error

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: './', // 使用相对路径，允许直接打开index.html
 });
